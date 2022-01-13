c752eee5a75c4187151bc8fd67ff1a67c759d6c2
LiViS-code <81714297+LiViS-code@users.noreply.github.com>
2022-01-13 11:50:54 +0200

    refactoring via hooks

--- feedback/src/components/NotificationMessage/NotificationMessage.jsx
+++ feedback/src/components/NotificationMessage/NotificationMessage.jsx
@@ -1,10 +1,4 @@
-import React, { Component } from "react";
 import { Message } from "./NotificationMessage.styled";
-
-class NotificationMessage extends Component {
-  render() {
-    return <Message>There is no feedback</Message>;
-  }
+export default function NotificationMessage() {
+  return <Message>There is no feedback</Message>;
 }
-
-export default NotificationMessage;
